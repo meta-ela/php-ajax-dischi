99a0bf2f4706fcc59aef921a70e9c3d2019b52db
meta-ela <anxhelameta@live.it>
2022-07-12 16:26:48 +0200

    Adds data db.php

--- template/card.php
+++ template/card.php
@@ -2,12 +2,10 @@
 
 ?>
 
-<div class="container py-5">
-    <div class="row row-cols-5 g-3">
-        <div class="col">
-            <div class="card py-5">
+<div class="row row-cols-5 g-3">
+    <div class="col">
+        <div class="card py-5">
 
-            </div>
         </div>
     </div>
 </div>
